0e94015847dfd836f6bca68cdd106843e3140e6a
李源隆 <2016011261@secoder.net>
2018-12-01 01:37:11 +0800

    Add controller and register

--- thinpad_top/thinpad_top.srcs/sources_1/new/Controller.v
+++ thinpad_top/thinpad_top.srcs/sources_1/new/Controller.v
@@ -1,13 +1,12 @@
 `timescale 1ns / 1ps
-
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer:  Jia Minglin
 // 
-// Create Date: 2018/11/30 19:43:11
+// Create Date: 2018/11/30 04:01:06
 // Design Name: 
 // Module Name: Controller
-// Project Name: 
+// Project Name: MIPS32 Computer
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -22,21 +21,166 @@
 
 
 module Controller(
-    input wire[31:0] Instruction,
-    output wire[1:0] EXResultSelect,
-    output wire RegWrite,
-    output wire MemRead,
-    output wire MemWrite,
-    output wire[1:0] BranchType,
-    output wire[1:0] JumpType,
-    output wire[5:0] RegSrcA,
-    output wire[5:0] RegSrcB,
-    output wire[5:0] RegDest,
-    output wire ALUSrc,
-    output wire MemToReg,
-    output wire[1:0] MemReadSelect,
-    output wire MemWriteSelect,
-    output wire IsMOVZ,
-    output wire[3:0] ALUOp
-    );
+	input wire[31:0] Instruction,
+	
+	output reg ExResultSelect,
+	output reg RegWrite,
+	output reg MemRead,
+	output reg MemWrite,
+	output reg[1:0] BranchType,
+	output reg[1:0] JumpType,
+	output reg[5:0] RegSrcA,
+	output reg[5:0] RegSrcB,
+	output reg[5:0] RegDest,
+	output reg ALUSrc,
+	output reg MemToReg,
+	output reg MemReadSelect,
+	output reg MemWriteSelect,
+	output reg IsMOVZ,
+	output reg[3:0] ALUOp
+	);
+	
+always@(*)begin
+	case (Instruction[31:26])
+		6'b0:begin
+			case (Instruction[5:0])
+				6'b100001://ADDU
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b0,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b0,2'b0,1'b0,1'b0,4'h3};
+				end
+				6'b100100://AND
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'h4};
+				end
+				6'b001000://JR
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b0,1'b0,1'b0,2'b0,2'b10,Instruction[25:21],Instruction[25:21],5'b0,1'b0,1'b0,2'b0,1'b0,1'b0,4'h0};
+				end
+				6'b100101://OR
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'h5};
+				end
+				6'b000000://SLL
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[20:16],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'h8};
+				end
+				6'b000010://SRL
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[20:16],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'h9};
+				end
+				6'b100110://XOR
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'h6};
+				end
+				6'b000011://SRA
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[20:16],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'ha};
+				end
+				6'b100111://NOR
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'h7};
+				end
+				6'b001010://MOVZ
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+					{2'b10,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b1,4'hc};
+				end
+			endcase
+		end
+		6'b001001 ://ADDIU
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b0,2'b0,1'b0,1'b0,4'h3};
+		end
+		6'b001100 ://ANDI 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b0,2'b0,1'b0,1'b0,4'h4};
+		end
+		6'b000100 ://BEQ 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b0,2'b1,2'b0,Instruction[25:21],Instruction[20:16],5'b0,1'b1,1'b0,2'b0,1'b0,1'b0,4'h0};
+		end
+		6'b000111 ://BGTZ 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b0,2'b11,2'b0,Instruction[25:21],Instruction[25:21],5'b0,1'b1,1'b0,2'b0,1'b0,1'b0,4'h0};
+		end
+		6'b000101 ://BNE 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b0,2'b10,2'b0,Instruction[25:21],Instruction[20:16],5'b0,1'b1,1'b0,2'b0,1'b0,1'b0,4'h0};
+		end
+		6'b000010 ://J 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b0,2'b0,2'b1,5'b0,5'b0,5'b0,1'b0,1'b0,2'b0,1'b0,1'b0,4'h0};
+		end
+		6'b000011 ://JAL
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,5'b0,5'b0,5'b0,1'b0,1'b0,2'b0,1'b0,1'b0,4'h0};
+		end
+		6'b100000 ://LB 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b1,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b1,2'b1,1'b0,1'b0,4'h3};
+		end
+		6'b001111 ://LUI
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b0,2'b0,2'b0,5'b0,5'b0,Instruction[20:16],1'b1,1'b0,2'b0,1'b0,1'b0,4'hd};
+		end
+		6'b100011 ://LW
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b1,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b1,2'b0,1'b0,1'b0,4'h3};
+		end
+		6'b001101 ://ORI
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b0,2'b0,1'b0,1'b0,4'h5};
+		end
+		6'b101000 ://SB
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b1,2'b0,2'b0,Instruction[25:21],Instruction[20:16],5'b0,1'b1,1'b0,2'b0,1'b1,1'b0,4'h3};
+		end
+		6'b101011 ://SW
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b0,1'b0,1'b1,2'b0,2'b0,Instruction[25:21],Instruction[20:16],5'b0,1'b1,1'b0,2'b0,1'b0,1'b0,4'h3};
+		end
+		6'b001110 ://XORI 
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b0,2'b0,1'b0,1'b0,4'h6};
+		end
+		6'b100100 ://LBU
+		begin
+			{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			{2'b00,1'b1,1'b1,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[25:21],Instruction[20:16],1'b1,1'b1,2'b10,1'b0,1'b0,4'h3};
+		end
+		6'b011100:begin
+			case (Instruction[5:0])
+				6'b100000://CLZ
+				begin
+					{ExResultSelect,RegWrite,MemRead,MemWrite,BranchType[1:0],JumpType[1:0],RegSrcA[5:0],RegSrcB[5:0],RegDest[5:0],ALUSrc,MemToReg,MemReadSelect,MemWriteSelect,IsMOVZ,ALUOp[3:0]}=
+			        {2'b00,1'b1,1'b0,1'b0,2'b0,2'b0,Instruction[25:21],Instruction[20:16],Instruction[15:11],1'b0,1'b0,2'b0,1'b0,1'b0,4'hb};
+				end
+			endcase
+		end
+		default:;
+	endcase;
+end
 endmodule
---
+++ thinpad_top/thinpad_top.srcs/sources_1/new/RegisterFile.v
@@ -0,0 +1,74 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Jia Minglin
+// 
+// Create Date: 2018/11/30 21:05:20
+// Design Name: 
+// Module Name: RegisterFile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module RegisterFile(
+
+    input wire clk,
+    input wire [5:0]ReadRegA,
+    input wire [5:0]ReadRegB,
+    input wire [31:0]NPCInput,
+    //input
+    input wire [5:0]WriteReg,
+    input wire [31:0]WriteData,
+    input wire RegWrite,
+    
+    //output
+    output reg [31:0]ReadDataA,
+    output reg [31:0]ReadDataB
+    
+    );
+reg [31:0] registers [0:31];
+always @(*)begin
+    if(ReadRegA[5]==0)
+        ReadDataA<=registers[ReadRegA];
+    else begin
+        if(ReadRegA[4:0]==0)
+            ReadDataA<=NPCInput;
+        else begin
+            ReadDataA<=0;
+        end
+    end
+
+    if(ReadRegB[5]==0)
+        ReadDataB<=registers[ReadRegB];
+    else begin
+        if(ReadRegB[4:0]==0)
+            ReadDataB<=NPCInput;
+        else begin
+            ReadDataB<=0;
+        end
+    end
+end
+
+always@(posedge clk)begin
+    if(RegWrite)
+        if(WriteReg[5]==0)
+            if(WriteReg[4:0]==0)
+                ;
+            else begin
+                registers[WriteReg]<=WriteData;
+            end
+        else begin
+        end
+    else
+        ;
+end
+endmodule
